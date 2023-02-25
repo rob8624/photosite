77fe558378f2ead588d4de5c0e8cf31fa050670d
Rob8624 <robert_melen@yahoo.co.uk>
2023-02-25 01:09:07 +0000

    added image model and image view

--- photosite/main/models.py
+++ photosite/main/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Image(models.Model):
+    timestamp = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(upload_to = 'images')
+    image_number = models.IntegerField
+    front_page = models.BooleanField()
--- photosite/main/views.py
+++ photosite/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import models
+from .models import Image
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    images_all = Image.objects.all()
+    images = images_all.filter(front_page=True)
+    return render(request,  "index.html", {"images": images})
