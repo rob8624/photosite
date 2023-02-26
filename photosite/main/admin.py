b9d24026cf4b0b27d24d43264e71cd62c8372015
Rob8624 <robert_melen@yahoo.co.uk>
2023-02-26 13:51:16 +0000

    added editable caption to image model and admin

--- photosite/main/admin.py
+++ photosite/main/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Image
 
+
+
 # Register your models here.
+
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'image', 'image_number', 'front_page',)
+
+    
+    
 admin.site.register(Image)
+
+
+
+
+
--- photosite/main/models.py
+++ photosite/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image as Image_pil, ExifTags
 
 # Create your models here.
 
@@ -7,3 +8,28 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images')
     image_number = models.IntegerField
     front_page = models.BooleanField()
+    caption = models.TextField(max_length=2000, null=True, blank=True, editable=True)
+
+    
+    if caption != caption:
+        
+        def save(self, *args, **kwargs):
+            super(Image, self).save(*args, **kwargs)
+
+            """Get EXIF"""
+            
+            im = Image_pil.open(self.image)
+            try:
+                info = im.getexif()[0x010e]
+                self.caption = info
+                print(self.caption)
+
+            
+                
+                super(Image, self).save(*args, **kwargs)
+            
+            except KeyError:
+                pass
+            super(Image, self).save(*args, **kwargs)
+            
+   
